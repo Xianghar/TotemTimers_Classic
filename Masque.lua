2cf1345d1531727901f61fc1583f2b32b4dea25e
Jesus <jahdesik@gmail.com>
2021-05-23 16:38:51 -0500

    Added simple Masque support

---
+++ Masque.lua
@@ -0,0 +1,19 @@
+﻿if select(2,UnitClass("player")) ~= "SHAMAN" then return end
+
+if not LibStub then return end
+local msq = LibStub("Masque", true)
+
+function TotemTimers.InitMasque()
+	if msq then
+		local group = msq:Group("TotemTimers")
+		for _,v in pairs(XiTimers.timers) do
+            group:AddButton(v.button)
+            group:AddButton(v.animation.button)
+        end
+        for i = 1,#TTActionBars.bars do
+            for j = 1,#TTActionBars.bars[i].buttons do
+                group:AddButton(TTActionBars.bars[i].buttons[j])
+            end
+        end
+	end
+end
